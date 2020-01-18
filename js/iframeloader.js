d920c47718c0e816ffe8d59c90816d1675cd03eb
Eskander Bejaoui <mail@eskander.tn>
2020-01-18 02:54:34 +0100

    IFrame loader js

---
+++ js/iframeloader.js
@@ -0,0 +1,12 @@
+var loadCounter = 0;
+
+var loaded = function () {
+    loadCounter += 1;
+
+    if (loadCounter === 2) {
+        $("iframe").attr("height", "300px");
+        $('html, body').stop().animate({
+            scrollTop: ($('iframe').offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+    }
+}
