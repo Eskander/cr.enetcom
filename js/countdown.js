b5981cf545bab41be2afdc37879a97a67ca22475
Eskander <skan.bejaoui@outlook.com>
2020-01-20 21:41:49 +0100

    Cleanup

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,5 @@
 var competitionDate = new Date("Feb 23, 2020 08:00:15").getTime();
-var registrationDate = new Date("Jan 20, 2020 20:00:15").getTime();
 
-// Countdown 1
 var x = setInterval(function () {
 
     var now = new Date().getTime();
@@ -17,25 +15,5 @@ var x = setInterval(function () {
     if (distance < 0) {
         clearInterval(x);
         $("#countdown").hide();
-    }
-}, 1000);
-
-// Countdown 2
-var y = setInterval(function () {
-
-    var now = new Date().getTime();
-    var distance = registrationDate - now;
-
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    $("#registration").html(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
-
-
-    if (distance < 0) {
-        clearInterval(x);
-        $("#registration").hide();
     }
 }, 1000);
